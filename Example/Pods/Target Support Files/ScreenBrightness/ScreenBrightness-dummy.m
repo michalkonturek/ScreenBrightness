ef21135b755e97399c29d3d3fe3e7cce8129b92e
Michal Konturek <michal.konturek@gmail.com>
2016-08-15 22:08:12 +0100

    Adds pod installation.

---
+++ Example/Pods/Target Support Files/ScreenBrightness/ScreenBrightness-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ScreenBrightness : NSObject
+@end
+@implementation PodsDummy_ScreenBrightness
+@end
---
+++ Example/Pods/Target Support Files/ScreenBrightness/ScreenBrightness-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double ScreenBrightnessVersionNumber;
+FOUNDATION_EXPORT const unsigned char ScreenBrightnessVersionString[];
+
